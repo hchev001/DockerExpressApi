fc9fee0122c61f758b91386f21ec7e349bbe90ef
Hamilton Chevez <hchev001@gmail.com>
2019-09-24 10:20:31 -0400

    added the connection setup to the mongodatabase

--- api/src/index.js
+++ api/src/index.js
@@ -2,9 +2,21 @@ import bodyParser from 'body-parser';
 import express from 'express';
 import config from 'config';
 import SampleRoutes from './routes/sample.routes';
+import mongoose from "mongoose";
+
+// Let mongoose use the same implementation
+// of promises as the rest of the app
+mongoose.Promise = global.Promise;
+
+// Connect to the mongo database
+// config/development.json or 
+// config/production.json depending on NODE_ENV environment
+// variable
+mongoose.connect(config.setup.db, { useNewUrlParser: true, useUnifiedTopology: true })
 
 let app = express();
 
+// configure the port for the node express api to use
 let port = config.setup.PORT;
 
 // Assume that if a request contains data it is encoded as JSON
@@ -18,6 +30,6 @@ SampleRoutes(app);
 
 app.listen(port);
 
-console.log(`Server is listneing on port ${port}`);
+console.log(`Server is listening on port ${port}`);
 
 export default app;
